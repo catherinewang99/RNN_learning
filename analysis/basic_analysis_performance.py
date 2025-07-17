33469233ea7602a61a67c55da3084e0431df08f3
Catherine Wang <catherinewang@DN2tevl6.SUNet>
2025-07-17 09:59:26 -0700

    add initial project files

---
+++ analysis/basic_analysis_performance.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 10 15:13:55 2025
+
+@author: catherinewang
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+n_epochs = 20
+
+path = '/Users/catherinewang/Desktop/RNN/Dual_ALM_RNN/dual_alm_rnn_logs/TwoHemiRNNTanh/train_type_modular/n_neurons_256_random_seed_0/n_epochs_{}/lr_1.0e-04_bs_256/sigma_input_noise_0.10_sigma_rec_noise_0.10/xs_left_alm_amp_1.00_right_alm_amp_3.00/init_cross_hemi_rel_factor_0.20/'.format(n_epochs)
