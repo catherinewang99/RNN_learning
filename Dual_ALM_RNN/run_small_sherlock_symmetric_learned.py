86048dd819b2aca06cbd0f3268dc30479977a18c
Catherine Wang <catherinewang@DN2tevjy.SUNet>
2025-09-19 17:24:21 -0700

    sherlock scripts

---
+++ Dual_ALM_RNN/run_small_sherlock_symmetric_learned.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+import torch
+from dual_alm_rnn_exp import DualALMRNNExp
+
+
+
+input_asym = [(1,0), (1,0.1), (1,0.2), (1,0.3), (1,0.4), (1,0.5), (1,1), (0.5,1), (0.4,1), (0.3,1), (0.2,1), (0.1,1), (0,1)] # Sane as BK
+
+# Initialize experiment
+exp = DualALMRNNExp()
+
+# Train RNN for each input asymmetry
+for seed in range(100):
+    for epoch in [1, 11]:
+        exp.configs['xs_left_alm_amp'] = 1.0
+        exp.configs['xs_right_alm_amp'] = 1.0
+        exp.configs['random_seed'] = seed
+        exp.configs['train_type'] = 'train_type_asymmetric_fixed_input_mult_seeds'
+        exp.configs['unfix_epoch'] = epoch
+        exp.train_type_modular_single_readout()
+
+
+# weights_dict = {}
+
+# for asym in input_asym:
+#     all_weights = []
+#     for seed in range(100):
+#         exp = DualALMRNNExp()
+#         exp.configs['xs_left_alm_amp'] = asym[0]
+#         exp.configs['xs_right_alm_amp'] = asym[1]
+#         exp.configs['random_seed'] = seed
+
+#         # For each asymmetry, print the logs path for the config
+#         exp.init_sub_path(exp.configs['train_type'])
+#         logs_path = os.path.join(exp.configs['logs_dir'], exp.configs['model_type'], exp.sub_path)
+#         # print(f"Input asymmetry {asym}: logs path = {logs_path}")
+
+#         weights = np.load(os.path.join(logs_path, 'readout_weights_epoch_final.npy'))
+#         # ratio = np.sum(np.abs(weights)[0, :exp.n_neurons//2]) / np.sum(np.abs(weights)[0, :])
+#         all_weights.append(weights)
+#     weights_dict[asym] = all_weights
+
+# np.save('small_rnn_sherlock_weights_fixed_input.npy', weights_dict)
+
