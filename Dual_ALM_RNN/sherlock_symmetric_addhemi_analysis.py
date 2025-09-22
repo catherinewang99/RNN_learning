929993fb991c259f4a9e736578f8ed73f8520d1e
Catherine Wang <catherinewang@DN52e6md.SUNet>
2025-09-22 10:32:24 -0700

    Add changes to viz

---
+++ Dual_ALM_RNN/sherlock_symmetric_addhemi_analysis.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import torch
+from dual_alm_rnn_exp import DualALMRNNExp
+
+exp = DualALMRNNExp()
+
+logs_path = '/home/users/cwang314/dual_alm_rnn_logs/TwoHemiRNNTanh_single_readout/train_type_modular_fixed_input_cross_hemi_mult_seeds/n_neurons_4_random_seed_{}/unfix_epoch_{}/n_epochs_40_n_epochs_across_hemi_0/lr_3.0e-03_bs_75/sigma_input_noise_0.10_sigma_rec_noise_0.10/xs_left_alm_amp_1.00_right_alm_amp_1.00/init_cross_hemi_rel_factor_0.20'
+
+
+
+
+
